d5878ef4b519d4cae4359dcac775cd690412b52a
charleswlees <contact@charleswlees.com>
2025-07-10 19:59:42 -0400

    Adding cors

--- api.py
+++ api.py
@@ -2,13 +2,16 @@
 # Test file for sending a ZIP file over the network via an API
 
 from flask import Flask, request, send_file
+from flask_cors import CORS
 from cli import generate_firmware
 
 
 app = Flask(__name__)
 
+CORS(app)
+
 # Firmware Endpoint
-@app.route("/firmware/<path:keyboard_name>", methods=["GET"])
+@app.route("/firmware/<path:keyboard_name>", methods=["PUT"])
 def get_categories(keyboard_name):
     try:
         return send_file(generate_firmware(keyboard_name)), 200
