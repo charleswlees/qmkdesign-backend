f4afbb3c055bbde2967db229778549430e4d9f75
charleswlees <contact@charleswlees.com>
2025-08-03 17:40:15 -0400

    Building out AWS setup

--- terraform/resources.tf
+++ terraform/resources.tf
@@ -1,9 +1,9 @@
-resource "aws_s3_bucket" "backups" {
+resource "aws_s3_bucket" "qmkdesign-backend-backup-bucket" {
   provider = aws.p1
-  bucket   = "backup-bucket"
+  bucket   = "qmkdesign-backend-backup-bucket"
 
   tags = {
-    Name = "backup-bucket"
+    Name = "qmkdesign-backend-backup-bucket"
   }
 }
 
@@ -19,3 +19,112 @@ resource "aws_dynamodb_table" "user-data" {
   }
 }
 
+resource "aws_ecr_repository" "qmkdesign-backend" {
+  provider = aws.p1
+  name = "qmkdesign-backend"
+}
+
+resource "aws_ecr_lifecycle_policy" "cleanup" {
+  provider   = aws.p1
+  repository = aws_ecr_repository.qmkdesign-backend.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Only keep 2 images in ECR"
+        selection = {
+          tagStatus   = "any"
+          countType   = "imageCountMoreThan"
+          countNumber = 2
+        }
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role" "lambda_role" {
+  provider = aws.p1
+  name     = "backend_api_lambda_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic" {
+  provider   = aws.p1
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  role       = aws_iam_role.lambda_role.name
+}
+
+resource "aws_iam_role_policy" "lambda_policy" {
+  provider = aws.p1
+  name     = "lambda_policy"
+  role     = aws_iam_role.lambda_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "dynamodb:*",
+          "s3:*",
+          "ecr:GetAuthorizationToken",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_lambda_function" "backend_lambda" {
+  provider      = aws.p1
+  function_name = "backend_api"
+  role          = aws_iam_role.lambda_role.arn  
+  
+  package_type = "Image"
+  image_uri    = "${aws_ecr_repository.qmkdesign-backend.repository_url}:latest"  
+  
+  timeout     = 30
+  memory_size = 128
+  
+  environment {
+    variables = {
+      DYNAMODB_TABLE = aws_dynamodb_table.user-data.name
+      S3_BUCKET      = aws_s3_bucket.qmkdesign-backend-backup-bucket.id
+    }
+  }
+  
+  depends_on = [
+    aws_iam_role_policy_attachment.lambda_basic,
+    aws_iam_role_policy.lambda_policy
+  ]
+}
+
+
+resource "aws_lambda_function_url" "api_url" {
+  provider           = aws.p1
+  function_name      = aws_lambda_function.backend_lambda.function_name
+  authorization_type = "NONE"  
+}
+
+output "function_url" {
+  value = aws_lambda_function_url.api_url.function_url
+}
